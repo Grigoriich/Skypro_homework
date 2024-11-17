63ab4ffaaff59d50b313bea583181e81ae6ae83e
Grigorii Chaikin <gnchaykin@mail.ru>
2024-11-17 22:14:18 +0700

    create funcs filter_by_state and sort_by_date

---
+++ src/processing.py
@@ -0,0 +1,16 @@
+from src.widget import get_date_format
+
+
+def filter_by_state(dictionary_list: list[dict], state: str='EXECUTED') -> list[dict]:
+    '''Функция, фильтрующая список словарей по ключу state'''
+    result_list = []
+    for item in dictionary_list:
+        if item["state"] == state:
+            result_list.append(item)
+    return result_list
+
+
+def sort_by_date(dictionary_list: list[dict], reverse_: bool=False) -> list[dict]:
+    '''Функция, сортирующая список словарей по дате, с возможностью изменения порядка сортировки'''
+    result_list = sorted(dictionary_list, reverse=reverse_, key=lambda x: get_date_format(x["date"]))
+    return result_list
