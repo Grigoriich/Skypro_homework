2df7b69885bc2062cf1ccb69c19ffc2af099d5f0
Grigorii Chaikin <gnchaykin@mail.ru>
2024-11-17 22:24:30 +0700

    sort_by_date change default bool

--- src/processing.py
+++ src/processing.py
@@ -10,7 +10,7 @@ def filter_by_state(dictionary_list: list[dict], state: str='EXECUTED') -> list[
     return result_list
 
 
-def sort_by_date(dictionary_list: list[dict], reverse_: bool=False) -> list[dict]:
+def sort_by_date(dictionary_list: list[dict], increase: bool=True) -> list[dict]:
     '''Функция, сортирующая список словарей по дате, с возможностью изменения порядка сортировки'''
-    result_list = sorted(dictionary_list, reverse=reverse_, key=lambda x: get_date_format(x["date"]))
+    result_list = sorted(dictionary_list, reverse=not increase, key=lambda x: get_date_format(x["date"]))
     return result_list
