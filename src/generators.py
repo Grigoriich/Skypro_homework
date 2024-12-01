a7ed9f647acbcdeab9177b675f8c40e275c3105f
Grigorii Chaikin <gnchaykin@mail.ru>
2024-12-01 15:28:54 +0700

    extra tests for invalid data

--- src/generators.py
+++ src/generators.py
@@ -3,6 +3,10 @@ from typing import Dict, List, Generator
 
 def filter_by_currency(transactions: List[Dict], currency_code: str) -> Generator:
     "Генераторная функция, возвращающая итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной (например, USD)"
+    if not transactions:
+        raise ValueError("Список транзакций пуст")
+    if currency_code not in [x["operationAmount"]["currency"]["code"] for x in transactions]:
+        raise ValueError("Операции с заданной валютой отсутствуют")
     for transaction in transactions:
         if transaction["operationAmount"]["currency"]["code"] == currency_code:
             yield transaction
@@ -10,6 +14,10 @@ def filter_by_currency(transactions: List[Dict], currency_code: str) -> Generato
 
 def transaction_descriptions(transactions: List[Dict]) -> str:
     "Генераторная функция, возвращающая итератор, который поочередно выдает описание каждой транзакции в текстовом формате"
+    if not transactions:
+        raise ValueError("Список транзакций пуст")
+    if "description" not in ["description" for x in transactions if "description" in x]:
+        raise ValueError("Описание транзакции отсутствует")
     for transaction in transactions:
         yield transaction["description"]
 
@@ -20,3 +28,5 @@ def card_number_generator(start: int, stop: int) -> str:
     for number in card_numbers:
         yield f"{number[0:4]} {number[4:8]} {number[8:12]} {number[12:]}"
 
+# print(next(filter_by_currency([], "USD")))
+# print(len([]))
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -13,14 +13,35 @@ def test_filter_by_currency_rub(transactions, transactions_rub):
         assert next(filtered_transaction) == transaction_rub
 
 
+@pytest.mark.parametrize("currency", ["eur", "yen", "kzt"])
+def test_filter_by_currency_miss(currency, transactions):
+    with pytest.raises(ValueError):
+        next(filter_by_currency(transactions, currency))
+
+
+def test_filter_by_currency_invalid():
+    with pytest.raises(ValueError):
+        next(filter_by_currency([], "USD"))
+
+
 def test_transaction_descriptions(transactions, fix_descriptions):
     transaction_description = transaction_descriptions(transactions)
     for description in fix_descriptions:
         assert next(transaction_description) == description
 
 
+def test_transaction_descriptions_miss():
+    with pytest.raises(ValueError):
+        next(transaction_descriptions([{"id": 939719570, "state": "EXECUTED",}]))
+
+
+def test_transaction_descriptions_invalid():
+    with pytest.raises(ValueError):
+        next(transaction_descriptions([]))
+
+
 def test_card_number_generator():
-    card_number = card_number_generator(9999, 10009)
+    card_number = card_number_generator(9999, 10001)
     assert next(card_number) == "0000 0000 0000 9999"
     assert next(card_number) == "0000 0000 0001 0000"
     assert next(card_number) == "0000 0000 0001 0001"
