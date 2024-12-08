eac7153c32964a8d259583fc0d4d5567ac953276
Grigorii Chaikin <gnchaykin@mail.ru>
2024-12-08 21:40:22 +0700

    lint fixes

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,11 +1,12 @@
 from src.decorators import log
 
+
 def test_decorator_log(capsys):
     @log(filename=None)
     def multiply(x, y):
-        return x*y
+        return x * y
 
-    multiply(2,3)
+    multiply(2, 3)
     captured = capsys.readouterr()
     assert captured.out == "multiply ok\n"
 
@@ -15,17 +16,18 @@ def test_decorator_log_exception(capsys):
     def multiply(x, y):
         raise ValueError("Недопустимые значения")
 
-    multiply(2,3)
+    multiply(2, 3)
     captured = capsys.readouterr()
     assert captured.out == "multiply error: Недопустимые значения. Inputs: (2, 3), {}\n"
+
 
 def test_decorator_log_to_file():
     @log(filename="text.txt")
     def multiply(x, y):
-        return x*y
+        return x * y
 
-    multiply(2,3)
-    with open("text.txt", 'r') as file:
+    multiply(2, 3)
+    with open("text.txt", "r") as file:
         content = file.readlines()
     assert content[-1] == "multiply ok\n"
 
@@ -35,7 +37,7 @@ def test_decorator_log_to_file_exception():
     def multiply(x, y):
         raise ValueError("Недопустимые значения")
 
-    multiply(2,3)
-    with open("text.txt", 'r') as file:
+    multiply(2, 3)
+    with open("text.txt", "r") as file:
         content = file.readlines()
     assert content[-1] == "multiply error: Недопустимые значения. Inputs: (2, 3), {}\n"
