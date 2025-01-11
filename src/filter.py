636ce2cba14c39425d0052e025ffdfe568b83da5
Grigorii Chaikin <gnchaykin@mail.ru>
2025-01-11 14:08:34 +0700

    create test_filter and fixes in test_generators

--- src/filter.py
+++ src/filter.py
@@ -1,31 +1,36 @@
 import re
 from collections import Counter
+from typing import Dict, List
 
 from src.utils import load_json_from_path
 
 
-def filter_by_string(operations_list: list, pattern: str) -> list:
+def filter_by_string(operations_list: List[Dict], pattern: str) -> List[Dict]:
     """Функция, фильтрующая список словарей транзакций по заданному описанию"""
     filtered_list = []
+    if pattern == "":
+        return operations_list
     for operation in operations_list:
-        if "description" not in operation and pattern == "":
-            return operations_list
+        if "description" not in operation:
+            continue
         elif "description" in operation and re.search(pattern.lower(), operation["description"].lower()):
             filtered_list.append(operation)
     return filtered_list
 
 
-def counter_by_category_list(operations_list: list, category_list: list) -> dict:
+def counter_by_category_list(operations_list: List[Dict], category_list: List) -> Dict:
     """Функция, возвращающая словарь, где ключи - заданные категории операций,
-    значения - количество операций категории в списке операций"""
+    значения - количество операций категории в списке транзакций"""
     descriptions_list = []
+    if category_list == []:
+        return {}
     for operation in operations_list:
-        if "description" not in operation and category_list == []:
-            return operations_list
+        if "description" not in operation:
+            continue
         elif "description" in operation and operation["description"] in category_list:
             descriptions_list.append(operation["description"])
 
-        counter_category_dict = dict(Counter(descriptions_list))
+    counter_category_dict = dict(Counter(descriptions_list))
     return counter_category_dict
 
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -189,3 +189,118 @@ def fix_descriptions():
         "Перевод с карты на карту",
         "Перевод организации",
     ]
+
+
+@pytest.fixture
+def transactions_without_description():
+    return [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702",
+        },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188",
+        },
+        {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160",
+        },
+        {
+            "id": 895315941,
+            "state": "EXECUTED",
+            "date": "2018-08-19T04:27:37.904916",
+            "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод с карты на карту",
+            "from": "Visa Classic 6831982476737658",
+            "to": "Visa Platinum 8990922113665229",
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657",
+        },
+    ]
+
+
+@pytest.fixture
+def counted_descriptions():
+    return {
+        "Перевод организации": 1,
+        "Перевод со счета на счет": 2,
+        "Перевод с карты на карту": 1,
+    }
+
+
+@pytest.fixture
+def transactions_from_csv_or_xlsx():
+    return [
+        {
+            "id": 650703.0,
+            "state": "EXECUTED",
+            "date": "2023-09-05T11:30:32Z",
+            "amount": 16210.0,
+            "currency_name": "Sol",
+            "currency_code": "PEN",
+            "from": "Счет 58803664561298323391",
+            "to": "Счет 39745660563456619397",
+            "description": "Перевод организации",
+        },
+        {
+            "id": 3598919.0,
+            "state": "EXECUTED",
+            "date": "2020-12-06T23:00:58Z",
+            "amount": 29740.0,
+            "currency_name": "Peso",
+            "currency_code": "COP",
+            "from": "Discover 3172601889670065",
+            "to": "Discover 0720428384694643",
+            "description": "Перевод с карты на карту",
+        },
+        {
+            "id": 593027.0,
+            "state": "CANCELED",
+            "date": "2023-07-22T05:02:01Z",
+            "amount": 30368.0,
+            "currency_name": "Shilling",
+            "currency_code": "TZS",
+            "from": "Visa 1959232722494097",
+            "to": "Visa 6804119550473710",
+            "description": "Перевод с карты на карту",
+        },
+    ]
+
+
+@pytest.fixture
+def transactions_from_csv_or_xlsx_tzs():
+    return [
+        {
+            "id": 593027.0,
+            "state": "CANCELED",
+            "date": "2023-07-22T05:02:01Z",
+            "amount": 30368.0,
+            "currency_name": "Shilling",
+            "currency_code": "TZS",
+            "from": "Visa 1959232722494097",
+            "to": "Visa 6804119550473710",
+            "description": "Перевод с карты на карту",
+        },
+    ]
